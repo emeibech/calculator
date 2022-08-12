ae33450c4bdc88d8dbadc2b73c77201031b99d3e
emeibech <emeibech@gmail.com>
2022-08-12 18:23:43 +0800

    Create operation functions inside an object

---
+++ script.js
@@ -0,0 +1,20 @@
+//functions for the operations
+const operations = {
+    add: function(a, b) {
+        return a + b;
+    }, 
+    subtract: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(dividend, divisor) {
+        return dividend / divisor;
+    }
+}
+
+
+
+
+console.log(operations.multiply(12, 12));
